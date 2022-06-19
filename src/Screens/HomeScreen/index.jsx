6cfef16c6c7e9cdb1e0fd8a811d5a3a6a12c2d6a
Ashok <mettaa.games@gmail.com>
2022-06-19 11:05:52 +0800

    added home page live data

--- src/Screens/HomeScreen/index.jsx
+++ src/Screens/HomeScreen/index.jsx
@@ -2,17 +2,26 @@ import { FlatList } from "react-native";
 import React from "react";
 import styles from './styles'
 import CoinItem from '../../components/CoinItem'
-import CryptoCurrencies from '../../../assets/data/cryptocurrencies.json'
+import CryptoCurrenciesDunp from '../../../assets/data/cryptocurrencies.json'
 import { useEffect, useState } from 'react';
+import {getCryptoCurrencies} from '../../services/CryptoServices'
 
 const HomeScreen = () => {
-    const [data, setData] = useState([]);
+    const [coinsList, setCoinsList] = useState([]);
+
+    const fetchCoinData = async () => {
+      const cryptoCurrencies = await getCryptoCurrencies();
+      console.log('-------fetchCoinData-----------')
+      console.log(cryptoCurrencies);
+      setCoinsList(cryptoCurrencies);
+    }
     useEffect(()=>{
+      fetchCoinData();
       }, [])
     
     return(
         <FlatList 
-          data={CryptoCurrencies}
+          data={coinsList}
           renderItem={({item}) => <CoinItem 
           coinData={item}
         />}
--- src/services/CryptoServices.jsx
+++ src/services/CryptoServices.jsx
@@ -1,5 +1,18 @@
  import axios from "axios"
 
+ export const getCryptoCurrencies = async () => {
+
+    try{
+        const API_BASE_URL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=20&page=1&sparkline=true&price_change_percentage=7d`
+        console.log(API_BASE_URL);
+        const response = await axios.get(API_BASE_URL);
+        return response.data
+    }catch(err){
+        console.log(err.message)
+    }
+
+}
+
 export const getCoinMarketData = async (coinId) => {
     try{
         const API_BASE_URL = `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=USD&days=1&interval=hourly`
